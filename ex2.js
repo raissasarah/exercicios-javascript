cc61902ceff2c7261c81e150d5c3339bb0b782ef
Raissa <raissa.info.cont@gmail.com>
2023-02-05 15:44:35 -0300

    Resolucao de exercicio 2 adicionado

---
+++ ex2.js
@@ -0,0 +1,7 @@
+/* 
+    Faça um Programa que peça um número e então mostre a mensagem:
+    O número informado foi [número]. 
+*/
+
+let numero = 15;
+console.log(`O número digitado foi ${numero}`);
